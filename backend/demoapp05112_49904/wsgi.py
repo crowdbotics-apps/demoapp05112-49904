5d1d4b27d78091c89638cebefd3a7fc4b7a5695a
crowdbotics-apps <team@crowdbotics.com>
2024-11-05 07:47:52 +0000

    Update models

--- backend/demoapp05112_49904/wsgi.py
+++ backend/demoapp05112_49904/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "demoapp05112_49904.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demoapp05112_49904.settings")
 
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -5,9 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "demoapp05112_49904.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demoapp05112_49904.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
